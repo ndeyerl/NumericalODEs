ba7157860467042db99e4036e1933f220c7120df
ndeyerl <ndeyerl@smu.edu>
2016-12-05 10:44:33 -0600

    imex project plots commit

---
+++ IMEXproject/plots.m
@@ -0,0 +1,142 @@
+%ARK3
+figure
+loglog(h,ark3p1(1:4),'b')
+hold on
+loglog(h,ark3p2(1:4),'r')
+loglog(h,ark3p3(1:4),'g')
+loglog(h,ark3p4(1:4),'m')
+loglog(h,ark3(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+
+figure
+loglog(h,ark3p1(5:8),'b')
+hold on
+loglog(h,ark3p2(5:8),'r')
+loglog(h,ark3p3(5:8),'g')
+loglog(h,ark3p4(5:8),'m')
+loglog(h,ark3(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+figure
+loglog(h,ark3p1(9:12),'b')
+hold on
+loglog(h,ark3p2(9:12),'r')
+loglog(h,ark3p3(9:12),'g')
+loglog(h,ark3p4(9:12),'m')
+loglog(h,ark3(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+figure
+loglog(h,ark3p1(13:16),'b')
+hold on
+loglog(h,ark3p2(13:16),'r')
+loglog(h,ark3p3(13:16),'g')
+loglog(h,ark3p4(13:16),'m')
+loglog(h,ark3(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+figure
+loglog(h,ark3p1(17:20),'b')
+hold on
+loglog(h,ark3p2(17:20),'r')
+loglog(h,ark3p3(17:20),'g')
+loglog(h,ark3p4(17:20),'m')
+loglog(h,ark3(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+
+%ARK4
+figure
+loglog(h,ark4p1(1:4),'b')
+hold on
+loglog(h,ark4p2(1:4),'r')
+loglog(h,ark4p3(1:4),'g')
+loglog(h,ark4p4(1:4),'m')
+loglog(h,ark4(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+
+figure
+loglog(h,ark4p1(5:8),'b')
+hold on
+loglog(h,ark4p2(5:8),'r')
+loglog(h,ark4p3(5:8),'g')
+loglog(h,ark4p4(5:8),'m')
+loglog(h,ark4(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+figure
+loglog(h,ark4p1(9:12),'b')
+hold on
+loglog(h,ark4p2(9:12),'r')
+loglog(h,ark4p3(9:12),'g')
+loglog(h,ark4p4(9:12),'m')
+loglog(h,ark4(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+figure
+loglog(h,ark4p1(13:16),'b')
+hold on
+loglog(h,ark4p2(13:16),'r')
+loglog(h,ark4p3(13:16),'g')
+loglog(h,ark4p4(13:16),'m')
+loglog(h,ark4(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+figure
+loglog(h,ark4p1(17:20),'b')
+hold on
+loglog(h,ark4p2(17:20),'r')
+loglog(h,ark4p3(17:20),'g')
+loglog(h,ark4p4(17:20),'m')
+loglog(h,ark4(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+
+%ARK5
+figure
+loglog(h,ark5p1(1:4),'b')
+hold on
+loglog(h,ark5p2(1:4),'r')
+loglog(h,ark5p3(1:4),'g')
+loglog(h,ark5p4(1:4),'m')
+loglog(h,ark5(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+
+figure
+loglog(h,ark5p1(5:8),'b')
+hold on
+loglog(h,ark5p2(5:8),'r')
+loglog(h,ark5p3(5:8),'g')
+loglog(h,ark5p4(5:8),'m')
+loglog(h,ark5(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+figure
+loglog(h,ark5p1(9:12),'b')
+hold on
+loglog(h,ark5p2(9:12),'r')
+loglog(h,ark5p3(9:12),'g')
+loglog(h,ark5p4(9:12),'m')
+loglog(h,ark5(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+figure
+loglog(h,ark5p1(13:16),'b')
+hold on
+loglog(h,ark5p2(13:16),'r')
+loglog(h,ark5p3(13:16),'g')
+loglog(h,ark5p4(13:16),'m')
+loglog(h,ark5(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
+
+figure
+loglog(h,ark5p1(17:20),'b')
+hold on
+loglog(h,ark5p2(17:20),'r')
+loglog(h,ark5p3(17:20),'g')
+loglog(h,ark5p4(17:20),'m')
+loglog(h,ark5(1:4),'k--')
+loglog(h,arkstage(1:4),'k--o')
